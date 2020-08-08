d6df55d99850e2cf5ed8c7724167a35ded6794de
diegokock <diegokock90@gmail.com>
2020-08-08 11:25:04 -0300

    commit inicial

---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import PageHeader from '../../components/pageHeader';
+
+import './styles.css';
+
+function TeacherList() {
+    return (
+        <div id="page-teacher-list" className="container">
+            <PageHeader />
+        </div>
+    )
+}
+export default TeacherList;
